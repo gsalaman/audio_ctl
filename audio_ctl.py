028ddd8aa0b507c0ab6a919acb6200c9f6a69ace
Glenn Salaman <g.salaman@comcast.net>
2019-09-11 15:24:41 -0600

    Add frequency control info

--- audio_ctl.py
+++ audio_ctl.py
@@ -37,6 +37,33 @@ def y_zoom_out():
   client.publish("display/time/y_ctl", "-")
   print ("zoom out on time y")
 
+def freq_mag_zoom_in():
+  global client
+  client.publish("display/freq/y_ctl", "+")
+  print "zoom in on freq magnitude"
+
+def freq_mag_zoom_out():
+  global client
+  client.publish("display/freq/y_ctl", "-")
+  print "zoom out on frequency magnitude"
+
+def send_pixels_per_bin():
+  global pixels_per_bin_slider
+  global client
+
+  my_str = str(pixels_per_bin_slider.get())
+  client.publish("display/freq/pixels_per_bin", my_str)
+  print ("requesting "+my_str+" pixels_per_bin")
+
+def send_num_pts_per_bin():
+  global num_pts_per_bin_slider
+  global client
+
+  my_str = str(num_pts_per_bin_slider.get())
+  client.publish("display/freq/num_pts_per_bin", my_str)
+  print ("requesting "+my_str+" points per bin")
+
+###############################################
 my_window=Tk()
 my_window.title("Audio Display Controls")
 
@@ -55,7 +82,7 @@ time_color_indexes.grid(row=1,column=1,columnspan=3)
 
 time_thickness_label = Label(frame_time,text="Thickness")
 time_thickness_label.grid(row=2,column=0)
-time_thickness_slider = Scale(frame_time,from_=1, to=10,orient=HORIZONTAL)
+time_thickness_slider = Scale(frame_time,from_=0, to=10,length=200,orient=HORIZONTAL)
 time_thickness_slider.grid(row=2,column=1,columnspan=3)
 time_thickness_button = Button(frame_time)
 time_thickness_button["text"] = "Send"
@@ -76,6 +103,34 @@ time_y_zoom_out_button = Button(frame_time, text="Zoom Out", command=y_zoom_out)
 time_y_zoom_out_button.grid(row=4,column=2)
 
 frame_time.grid(row=0,column=0)
+
+frame_freq=LabelFrame(my_window,text="Frequency Display adjustments")
+
+freq_y_label = Label(frame_freq, text="magnitude")
+freq_y_label.grid(row=0,column=0)
+freq_y_zoom_in_button = Button(frame_freq, text="Zoom In", command=freq_mag_zoom_in)
+freq_y_zoom_in_button.grid(row=0,column=1)
+freq_y_zoom_out_button = Button(frame_freq, text="Zoom Out", command=freq_mag_zoom_out)
+freq_y_zoom_out_button.grid(row=0,column=2)
+
+pixels_per_bin_label = Label(frame_freq,text="Pixels per bin")
+pixels_per_bin_label.grid(row=1,column=0)
+pixels_per_bin_slider = Scale(frame_freq,from_=1,to=10,length=200,orient=HORIZONTAL)
+pixels_per_bin_slider.grid(row=1,column=1,columnspan=3)
+pixels_per_bin_button = Button(frame_freq)
+pixels_per_bin_button["text"] = "Send"
+pixels_per_bin_button["command"] = send_pixels_per_bin
+pixels_per_bin_button.grid(row=1,column=4)
+
+num_pts_per_bin_label = Label(frame_freq,text="Num pts per bin")
+num_pts_per_bin_label.grid(row=2,column=0)
+num_pts_per_bin_slider = Scale(frame_freq,from_=1,to=20,length=200,orient=HORIZONTAL)
+num_pts_per_bin_slider.grid(row=2,column=1,columnspan=3)
+num_pts_per_bin_button = Button(frame_freq)
+num_pts_per_bin_button["text"] = "Send"
+num_pts_per_bin_button["command"] = send_num_pts_per_bin
+num_pts_per_bin_button.grid(row=2,column=4)
+frame_freq.grid(row=1,column=0)
 
 #####################################################
 # Message callback for MQTT
